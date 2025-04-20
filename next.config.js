17959fdda74ee46744cdbb468a0ee169e40ba7fd
sha <sharadchaturveda@gmail.com>
2025-04-20 11:17:18 +0530

    11:17 20-04

--- next.config.js
+++ next.config.js
@@ -6,8 +6,9 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  // Disable experimental CSS optimization to avoid critters dependency issues
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
 };
 
